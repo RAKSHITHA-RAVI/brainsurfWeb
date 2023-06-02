b8d4cb5944c9e1b27e2ee6dc447ebb858cad80e5
Preethi V Hiremath <preethivhiremath.vh@gmail.com>
2023-06-02 18:39:46 +0530

    review1

--- brainsurf_Capstone/brainsurfWeb/app.py
+++ brainsurf_Capstone/brainsurfWeb/app.py
@@ -8,16 +8,10 @@ app = Flask(__name__, template_folder="templates")
 def home():
     return render_template("landing.html")  # Use your own HTML design for the home page
 
-
-# @app.route("/documentation")
-# def documentation():
-#     return render_template("documentation.html")  # Use your own HTML design for the documentation page
-
-
 @app.route("/ide")
 def ide():
     subprocess.Popen(["streamlit", "run", "streamlit_app.py"])
-    return render_template("templates\streaming_html.html")
+    return render_template("streaming_html.html")
 
 
 if __name__ == "__main__":
